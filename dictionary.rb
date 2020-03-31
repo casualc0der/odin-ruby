301455c11e4587025b3f162b06f0fba9c5dced4b
Edd Sansome <edd@murismo.com>
2020-03-31 13:40:31 +0100

    Clean up dictionary solution

--- dictionary.rb
+++ dictionary.rb
@@ -1,33 +1,17 @@
 #frozen_string_literal: true
 
 def substrings(string, dic)
-
-counter = {}
-string.split(' ').each do |word|
-dic.each do |e|
-  if word.downcase.include?(e)
-    if counter[e] == nil
-      counter[e] = 1
-    else
-      counter[e] +=1
+  counter = {}
+    string.split(' ').each do |word|
+      dic.each do |e|
+        if word.downcase.include?(e)
+          counter[e] == nil ? counter[e] = 1 : counter[e] +=1
+        end
+      end
     end
-  end
-end
-end
-counter
+  counter
 end
 
-dictionary = ['hell', 'my', 'name',
-   'is', 'hello', 'edd', 'ed', 'he']
-sentence = "Hello my name is Edd Eddie"
-
-=begin
-returns 
-{
-  "hell"=>1, "hello"=>1, "he"=>1, 
-  "my"=>1, "name"=>1, "is"=>1,
-   "edd"=>2, "ed"=>2
-}
-=end
-
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+sentence = "Howdy partner, sit down! How's it going?"
 p substrings(sentence, dictionary)
