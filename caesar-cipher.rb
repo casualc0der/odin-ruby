538c29c7886d122e6616f4165abe913c3ddc903f
Edd Sansome <edd@murismo.com>
2020-03-31 07:59:22 +0100

    Initial commit

---
+++ caesar-cipher.rb
@@ -0,0 +1,17 @@
+#frozen_string_literal: true
+
+def caeser(word, shift=1)
+
+  cipher = word.split('').map! do |letter|
+    if letter.match(/[A-Z]/)
+      (letter.ord - 64 + shift % 26 + 64).chr
+    elsif letter.match(/[a-z]/)
+      (letter.ord - 96 + shift % 26 + 96).chr
+    else
+      letter   
+    end  
+  end
+cipher.join
+end
+
+p caeser("Edd is Cool!!",1)
