d5cb3deb42b8e0e88b7b50ea4c2699c16533c749
casualc0der <edwardsansome@gmail.com>
2020-05-18 14:27:48 +0100

    clean up solution

--- bubblesort/bubblesort.rb
+++ bubblesort/bubblesort.rb
@@ -2,16 +2,16 @@ class Bubble
   def self.sort(arr)
     raise ArgumentError.new("Arrays only please!") unless arr.is_a?(Array)
     n = arr.length
-    until n <= 1
-      swaps = 0
-      for i in (1..n-1)
-        if arr[i-1] > arr[i]
-          arr[i-1], arr[i] = arr[i], arr[i-1]
-          swaps = i
-        end
+      until n <= 1
+        swaps = 0
+          for i in (1..n-1)
+            if arr[i-1] > arr[i]
+            arr[i-1], arr[i] = arr[i], arr[i-1]
+            swaps = i
+            end
+          end
+        n = swaps
       end
-      n = swaps
-    end
     arr
   end
 end
