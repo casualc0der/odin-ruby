fdb7f37c4ab4b4349eb1ef207b018df31a0a4e90
casualc0der <edwardsansome@gmail.com>
2020-05-21 21:01:32 +0100

    Add hangman class

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,25 +1,43 @@
 require_relative 'serializer'
 require_relative 'word_selector'
+require_relative 'dictionary'
 
-serializer = Serializer.new
+class Hangman
+  attr_reader(
+   :word_to_guess,
+   :guesses,
+   :letters_used,
+   :user_word
+  )
 
-class Donkey
   def initialize
-    @legs = 4
-    @head = 1
+    @serializer = Serializer.new
+    @word_grabber  = WordSelector.new(Dictionary.new)
+    @word_to_guess = @word_grabber.random.downcase
+    @guesses = 0
+    @letters_used = []
+    @user_word = Array.new(@word_to_guess.length)
   end
 
   def save_status(filename)
-    donkey = {gameid: filename, legs: @legs, head: @head }
+    game = { 
+      gameid: filename,
+ wordToGuess: word_to_guess,
+     guesses: guesses,
+ lettersUsed: letters_used,
+    userWord: user_word
+               }
+  end
+
+  def game_test
+      @serializer.serialize(self.save_status('EddsGame'))
+      file = File.read('game.json')
+      data = @serializer.deserialize(file)
   end
 end
 
-dopey = Donkey.new
-serializer.serialize(dopey.save_status('EddsGame'))
+dopey = Hangman.new
+dopey.game_test
 
-file = File.read('game.json')
-data = serializer.deserialize(file)
 
-p data.class
-p data
 
