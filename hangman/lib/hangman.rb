4b89a0a2d8f661c49e9ede39528c06f2cf313883
casualc0der <edwardsansome@gmail.com>
2020-05-22 11:58:18 +0100

    Basic game engine completed

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,6 +1,7 @@
 require_relative 'serializer'
 require_relative 'word_selector'
 require_relative 'dictionary'
+require 'pry'
 
 class Hangman
   attr_accessor(
@@ -36,26 +37,80 @@ class Hangman
       loaded_data = data[game_selection]
       load_game(loaded_data)
       puts "Game loaded, lets play!"
-      game_loop
+      game_loop(true)
     else
       puts "Ok lets play a new game"
-      game_loop
+      game_loop(false)
     end
   end
 
   private
 
-  def game_loop
+  def game_loop(loaded)
+    was_game_loaded = loaded
   while @guesses < 6
-    save_prompt if guesses >= 1
-    
-    puts "please take a guess"
-    guess_for_turn = gets.chomp
-    #do not allow input if player has already uses a letter
-    @guesses +=1 
+    display_current_status
+    save_prompt if guesses >= 1 && was_game_loaded == false
+    display_used_letters
+    char_checker_and_update_user_word(guess_char)
+    if win_checker?
+      p "You win, the word was #{@word_to_guess}!"
+      exit
+    end
+    was_game_loaded = false
   end
-  puts "Game over!"
+  puts "You have used all of your guesses! Game over!"
+  puts "The word was #{@word_to_guess}"
   exit
+  end
+
+  def win_checker?
+    @word_to_guess == @user_word.join
+  end
+
+  def display_current_status
+    asterisks = @user_word.map {|x| x.nil? ? x = '*' : x }.join('|')
+    puts asterisks
+  
+  end
+
+  def char_checker_and_update_user_word(guess)
+    if @word_to_guess.include?(guess)
+      puts "correct!"
+      locater = @word_to_guess.split('')
+                    .each_with_index  {|x, i|
+                       @user_word[i] = guess if x == guess }
+    else
+      puts "bzzz wrong!"
+      @guesses +=1
+      puts "You have #{6 - @guesses} incorrect guesses remaining" if guesses < 6
+    end
+  end
+
+  def guess_char
+    loop do
+      begin
+        puts "Please enter your guess for the turn"
+        input = gets.chomp
+        guess_for_turn = sanitize_char(input)
+        @letters_used << guess_for_turn
+        return guess_for_turn
+        rescue EncodingError => e
+        puts e.message
+        next
+      end
+    end
+  end
+
+  def sanitize_char(input)
+    raise EncodingError, 'Please enter 1 letter' if input.length != 1
+    raise EncodingError,'Please enter a letter' unless input.match(/\D/)
+    raise EncodingError, 'You have used that letter!' if @letters_used.include?(input)
+    input.downcase
+  end
+
+  def display_used_letters
+    puts "Letters used = " + @letters_used.join('|')
   end
 
   def save_prompt
